477d8fe66e4cc4d6d67c186ab84e8690d07ae818
dairymateo <seoxyt15@gmail.com>
2023-01-22 00:49:34 -0500

    xd

---
+++ generadorAleatorio.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    FILE *Gen=NULL;
+    Gen=fopen("GastosYIngresos.txt", "w");
+
+
+     if (Ing==NULL){
+        printf("No se puede abrir el archivo");
+        return -1;
+    }
+
+    int menor = 45, numero;
+    srand(time(NULL));
+
+    int cont=1;
+
+    for (int i=0; i<10; i++){
+        numero = rand() % 45;
+        count << numero ",";
+        
+    }
+    _getch();
+
+
+
+}
